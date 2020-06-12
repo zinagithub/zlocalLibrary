ff466b138ec3c671f24cd0b99b6d4fb74de0d328
Talaa Zina <talazina@gmail.com>
2020-06-12 18:03:56 +0100

    displaying library data (home-books-book_instances)

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+var moment = require('moment');
+
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -16,6 +18,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
