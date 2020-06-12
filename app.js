6a6cf59314079be902ab377193b71aeb8bc72182
Talaa Zina <talazina@gmail.com>
2020-06-12 17:16:39 +0100

    add routes and controllers fonctionalities

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb+srv://zinaMongo:z**m$$1973@cluster0-typlj.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//===========
+
 var createError = require('http-errors');
 
 var path = require('path');
@@ -14,9 +26,11 @@ const indexRouter = require('./routes/index');
 
 const usersRouter = require('./routes/users');
 
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -25,24 +39,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(indexRouter);
-app.use(usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
-  });
+});
   
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req,res){
-    res.send('Index page')
-})
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
+});
+
 
 module.exports = router;
