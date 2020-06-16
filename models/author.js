b7b62495a931efa8f34fa325c8d1aa792464ec98
Talaa Zina <talazina@gmail.com>
2020-06-16 18:23:12 +0100

    add author changes and bookinstance details

--- models/author.js
+++ models/author.js
@@ -31,10 +31,25 @@ AuthorSchema
 });
 
 // Virtual for author's lifespan
-AuthorSchema
-.virtual('lifespan')
-.get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifetime_string = '';
+  if (this.date_of_birth) {
+    lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY');
+  }
+  lifetime_string += ' - ';
+  if (this.date_of_death) {
+    lifetime_string += moment(this.date_of_death).format('MMMM Do, YYYY');
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+  return moment(this.date_of_death).format('YYYY-MM-DD');
 });
 
 // Virtual for author's URL
